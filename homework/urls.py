9c43f6fabe9d98920f0e78f9314ad38bbbfd021d
Насвищук Евгений <deletefromoganer@list.ru>
2021-11-27 02:51:18 +0700

    fixed

--- homework/urls.py
+++ homework/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
     path('articles/', include('articles.urls')),
-    path('pages/', include('pages.urls')),
+    path('', include('pages.urls')),
 ]
